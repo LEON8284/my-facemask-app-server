0aaf58479dddb260917e95a463b53c14d8d85208
TAIFUL KHAN <taifulleon@gmail.com>
2020-11-25 23:10:38 -0400

    My first commit for my face mask app server

---
+++ data.js
@@ -0,0 +1,56 @@
+let products = [
+  {
+    id: 1,
+    imageUrl: "",
+    name: "Disposable Mask",
+    available_quantity: 5,
+    price: 10,
+    description:
+      "Coronavirus protection. Blue antiviral disposable mask with ear loops on white background.",
+  },
+  {
+    id: 2,
+    imageUrl: "",
+    name: "N95 Mask",
+    available_quantity: 10,
+    price: 450,
+    description:
+      "filtering face mask for covid-19 - protection factor for N95 - reduces wearer’s exposure to particles including small particle aerosols and large droplets - safty mask on white background",
+  },
+  {
+    id: 3,
+    imageUrl: "",
+    name: "Mask for Kids",
+    available_quantity: 10,
+    price: 30,
+    description:
+      "Kids wearing the mask for protect them self from virus and air pollution. Prevention by mask to reduce spread of the coronavirus (covid-19) outbreak from human to human transmission.",
+  },
+  {
+    id: 4,
+    imageUrl: "",
+    name: "Reusuable Mask",
+    available_quantity: 8,
+    price: 48,
+    description:
+      "Zero waste anti virus mask. Handmade facemask from cotton natural fabric.",
+  },
+  {
+    id: 5,
+    imageUrl: "",
+    name: "Respirator Mask",
+    available_quantity: 10,
+    price: 750,
+    description: "Dust respirator masks",
+  },
+  {
+    id: 6,
+    imageUrl: "",
+    name: "Surgical Mask",
+    available_quantity: 100,
+    price: 30,
+    description: "Three ply surgical mask in box",
+  },
+];
+
+module.exports = { products: products };
