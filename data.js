95b268813d6e50acf8a705da1ce3c4424fb71edc
TAIFUL KHAN <taifulleon@gmail.com>
2020-11-26 21:16:55 -0400

    data updated

--- data.js
+++ data.js
@@ -36,16 +36,6 @@ let products = [
   {
     id: 4,
     imageUrl:
-      "https://photos.flickrprints.com/photos/i-L47vF3t/0/L/i-L47vF3t-L.jpg",
-    name: "Reusuable Mask",
-    available_quantity: 8,
-    price: 480,
-    description:
-      "Zero waste anti virus mask. Handmade facemask from cotton natural fabric.",
-  },
-  {
-    id: 5,
-    imageUrl:
       "https://photos.flickrprints.com/photos/i-c69D5NK/0/L/i-c69D5NK-L.jpg",
     name: "Respirator Mask",
     available_quantity: 10,
@@ -53,12 +43,22 @@ let products = [
     description: "Dust respirator masks",
   },
   {
-    id: 6,
+    id: 5,
     imageUrl:
       "https://photos.flickrprints.com/photos/i-SVL3Jsf/0/L/i-SVL3Jsf-L.jpg",
     name: " High quality Mask",
     available_quantity: 50,
     price: 10,
+    description:
+      "Coronavirus protection. Blue antiviral disposable mask with ear loops on white background.",
+  },
+  {
+    id: 6,
+    imageUrl:
+      "https://photos.flickrprints.com/photos/i-SVL3Jsf/0/L/i-SVL3Jsf-L.jpg",
+    name: " Easy Mask",
+    available_quantity: 60,
+    price: 25,
     description:
       "Coronavirus protection. Blue antiviral disposable mask with ear loops on white background.",
   },
@@ -76,9 +76,9 @@ let products = [
     id: 8,
     imageUrl:
       "https://photos.flickrprints.com/photos/i-SVL3Jsf/0/L/i-SVL3Jsf-L.jpg",
-    name: "Safe Mask",
-    available_quantity: 500,
-    price: 50,
+    name: " Easy Mask",
+    available_quantity: 60,
+    price: 25,
     description:
       "Coronavirus protection. Blue antiviral disposable mask with ear loops on white background.",
   },
