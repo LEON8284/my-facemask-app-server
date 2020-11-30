f391237b52a8997e6ab79d689514a6bba6fb36cc
TAIFUL KHAN <taifulleon@gmail.com>
2020-11-30 14:30:31 -0400

    adding disposable mask product information

--- data.js
+++ data.js
@@ -29,7 +29,7 @@ let products = [
       "https://photos.flickrprints.com/photos/i-5LhCdBB/0/L/i-5LhCdBB-L.jpg",
     name: "Mask for Kids",
     available_quantity: 10,
-    price: 300,
+    price: 30,
     description:
       "Kids wearing the mask for protect them self from virus and air pollution. Prevention by mask to reduce spread of the coronavirus (covid-19) outbreak from human to human transmission.",
   },
@@ -84,4 +84,72 @@ let products = [
   },
 ];
 
+/**
+ * This is a the products that we will send  to our React Face Mask Disposable Mask product page
+ */
+
+let disposableMask = [
+  {
+    id: 1,
+    imageUrl:
+      "https://photos.flickrprints.com/photos/i-SVL3Jsf/0/L/i-SVL3Jsf-L.jpg",
+    name: "Disposable Mask",
+    available_quantity: 100,
+    price: 25,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis augue neque, porttitor ut placerat eget, commodo et tellus.",
+  },
+  {
+    id: 2,
+    imageUrl:
+      "https://photos.flickrprints.com/photos/i-SVL3Jsf/0/L/i-SVL3Jsf-L.jpg",
+    name: "Disposable Mask",
+    available_quantity: 100,
+    price: 25,
+    description:
+      "Maecenas semper aliquet nunc, id luctus elit consectetur in. Etiam porttitor aliquam ligula, in varius diam. Vivamus rhoncus bibendum.",
+  },
+  {
+    id: 3,
+    imageUrl:
+      "https://photos.flickrprints.com/photos/i-SVL3Jsf/0/L/i-SVL3Jsf-L.jpg",
+    name: "Disposable Mask",
+    available_quantity: 100,
+    price: 25,
+    description:
+      "Donec volutpat finibus tortor pharetra accumsan. Proin porttitor feugiat accumsan. Integer ligula velit, euismod a est quis.",
+  },
+  {
+    id: 4,
+    imageUrl:
+      "https://photos.flickrprints.com/photos/i-SVL3Jsf/0/L/i-SVL3Jsf-L.jpg",
+    name: "Disposable Mask",
+    available_quantity: 100,
+    price: 25,
+    description:
+      "Proin at urna ut sem sodales luctus et vitae quam. Fusce nec efficitur justo, in ullamcorper lacus.",
+  },
+  {
+    id: 5,
+    imageUrl:
+      "https://photos.flickrprints.com/photos/i-SVL3Jsf/0/L/i-SVL3Jsf-L.jpg",
+    name: "Disposable Mask",
+    available_quantity: 100,
+    price: 25,
+    description:
+      "Phasellus consectetur augue lectus, vel condimentum dolor blandit condimentum.",
+  },
+  {
+    id: 6,
+    imageUrl:
+      "https://photos.flickrprints.com/photos/i-SVL3Jsf/0/L/i-SVL3Jsf-L.jpg",
+    name: "Disposable Mask",
+    available_quantity: 100,
+    price: 25,
+    description:
+      "nec ultricies. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+  },
+];
+
 module.exports = { products: products };
+module.exports = { disposableMask: disposableMask };
