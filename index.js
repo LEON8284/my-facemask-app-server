a736f9f22e9e3d18344a1250ba2e48b2f6cbb224
TAIFUL KHAN <taifulleon@gmail.com>
2020-12-03 22:09:40 -0400

    Stable_Version_03_12_2020

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let app = express();
 let bodyParser = require("body-parser");
 let cors = require("cors");
 let data = require("./data");
+let disposableMask = require("./disposableMask");
 let port = 4000;
 
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.get("/", (req, res) => res.send("Hello World"));
 
 app.get("/products", (req, res) => res.json(data.products)); // if we visit http://localhost:4000/products we will see the products information from the data.js file
 
-//app.get("/disposableMask", (req, res) => res.json(data.disposableMask)); // if we visit http://localhost:4000/disposableMask we will see the data for disposableMask from the data.js file
+app.get("/disposable", (req, res) => res.json(disposableMask.disposable)); // if we visit the http://localhost:4000/disposable see will see the disposable mask product information from disposableMask. js file
 
 app.listen(port);
 console.log("api running on " + port + ":");
